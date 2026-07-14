3fca6b2d9a7baa8c91080eddfc6b94c48317e188
Dindin Hernawan <dindin_hernawan@yahoo.com>
2026-07-14 16:13:36 +0700

    Pembaharuan FP Team

--- gitpush.sh
+++ gitpush.sh
@@ -13,7 +13,7 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>
 
 package=hacktrackgnulinux.github.io
-version="Pembaharuan Download RC21"
+version="Pembaharuan FP Team"
 figlet $package
 echo " Set Remote... "
 git remote set-url origin git@github.com:hacktrackgnulinux/$package.git
